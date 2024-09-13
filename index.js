4f535ee56b44b16250822b1779129823f102d734
vivek00s <126669757+vivek00s@users.noreply.github.com>
2024-09-13 12:56:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+let allContent = document.getElementById('content');
+let isClicked = false; //hide initially
+
+function hideDataShow() {
+    if (isClicked) {
+        allContent.style.display = 'none'; // Hide content
+        isClicked = false;
+    } else {
+        allContent.style.display = 'block'; // Show content
+        isClicked = true;
+    }
+}
+
+let allContent2 = document.getElementById('content2');
+let isClicked2 = false; //hide initially
+
+function hideDataShow2() {
+    if (isClicked2) {
+        allContent2.style.display = 'none'; // Hide content
+        isClicked2 = false;
+    } else {
+        allContent2.style.display = 'block'; // Show content
+        isClicked2 = true;
+    }
+}
+
+let allContent3 = document.getElementById('content3');
+let isClicked3 = false; //hide initially
+
+function hideDataShow3() {
+    if (isClicked3) {
+        allContent3.style.display = 'none'; // Hide content
+        isClicked3 = false;
+    } else {
+        allContent3.style.display = 'block'; // Show content
+        isClicked3 = true;
+    }
+}
+
+let allContent4 = document.getElementById('content4');
+let isClicked4 = false; //hide initially
+
+function hideDataShow4() {
+    if (isClicked4) {
+        allContent4.style.display = 'none'; // Hide content
+        isClicked4 = false;
+    } else {
+        allContent4.style.display = 'block'; // Show content
+        isClicked4 = true;
+    }
+}
